ddde20b3287bcb893e35deeba9f5823245cb9325
Anastasiya Kataeva <anastasiya.kataev@yandex.ru>
2022-10-08 01:18:44 +0300

    Выполнено задание 2

--- js-2(lesson2).js
+++ js-2(lesson2).js
@@ -1,23 +1,4 @@
-// const goods = [
-//     { title: 'Shirt', price: 150 },
-//     { title: 'Socks', price: 50 },
-//     { title: 'Jacket', price: 350 },
-//     { title: 'Shoes', price: 250 },
-//     { title: 'Jeans', price: 50 },
-//     { title: 'Skirt', price: 350 },
-//     { title: 'Cap', price: 250 },
-// ];
-// const renderGoodsItem = (title, price) => {
-//     return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-// };
-// const renderGoodsList = (list) => {
-//     let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-//     document.querySelector('.goods-list').innerHTML = goodsList.join(" ");
-// }
-//
-// renderGoodsList(goods);
-
-class GoodsItems {
+class GoodItem {
     constructor(title, price) {
         this.title = title;
         this.price = price;
@@ -30,8 +11,8 @@ class GoodsItems {
 class GoodsList {
     constructor() {
         this.goods = [];
-
     }
+
     fetchGoods(){
         this.goods = [
             { title: 'Shirt', price: 150 },
@@ -46,11 +27,38 @@ class GoodsList {
 
     render () {
         let listHtml = "";
-        this.goods.forEach(good => {
-            const  goodItem = new GoodsItems(good.title, good.price);
+        this.goods.forEach(function (good) {
+            const  goodItem = new GoodItem(good.title, good.price);
             listHtml += goodItem.render();
         })
         document.querySelector('.goods-list').innerHTML = listHtml;
+    }
+
+    getTotalprice() {
+        let totalPrice = 0;
+        this.goods.forEach(function(good) {
+            totalPrice += good.price;
+        })
+        return totalPrice;
+    }
+}
+
+class Cart {
+    addCartItem(cartItem) {
+    }
+    removeCartItem(cartItem) {
+    }
+}
+
+class CartItem {
+    goodItem = null;
+
+    constructor(goodItem) {
+        if (typeof goodItem !== GoodItem) {
+            console.error('The type of argument goodItem is not GoodItem class.')
+        }
+
+        this.goodItem = goodItem;
     }
 }
 
